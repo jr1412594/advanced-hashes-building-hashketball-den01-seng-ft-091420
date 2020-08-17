29732d45893d4038f73931c36b8687eaf694dd65
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-17 15:59:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -80,7 +80,15 @@ def game_hash
           
         },
         {
-          player_name: "Bismack "
+          player_name: "Bismack Biyombo",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 22,
+          blocks: 15,
+          slam_dunks: 10
         }]
     }
     

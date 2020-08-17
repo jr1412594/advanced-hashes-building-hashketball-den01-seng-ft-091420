1bde74ad95366a274c428d033717f3a383a39986
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-17 16:05:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -111,6 +111,17 @@ def game_hash
           steals: 1,
           blocks: 1,
           slam_dunks: 0
+        },
+        {
+          player_name: "Kemba walker",
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 7,
+          blocks: 5,
+          slam_dunks: 12
         }]
     }
     

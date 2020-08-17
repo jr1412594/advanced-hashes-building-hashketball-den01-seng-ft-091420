3425cd5acd0cc61fce52e3dcb4879bf438505b88
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-17 15:55:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -66,7 +66,19 @@ def game_hash
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
-      :players => [{}]
+      :players => [
+        {
+          player_name: "Jeff Adrien",
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          sla_dunks: 2
+          
+        }]
     }
     
   }
